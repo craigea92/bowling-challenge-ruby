9e92df4198d8f5d71f83174ad11210cedbe7f417
Craig Eaton <craigea92@gmail.com>
2021-07-04 22:00:26 +0100

    Written Tests & Added Web Helpers

---
+++ spec/bowling_spec.rb
@@ -0,0 +1,38 @@
+require 'bowling'
+require 'web_helpers'
+
+describe Bowling do
+  let(:game) { Bowling.new }
+
+  it 'can score a gutter game' do
+    20.times { game.roll(0) }
+    expect(game.score).to eq 0
+  end
+
+  it 'can score a game of all 1s' do
+    20.times { game.roll(1) }
+    expect(game.score).to eq 20
+  end
+
+  it 'can score a spare round' do
+    roll_spare
+    game.roll(3)
+    17.times { game.roll(0) }
+    expect(game.score).to eq 16
+  end
+  it 'can score a strike' do
+    game.roll(10)
+    game.roll(5)
+    game.roll(2)
+    16.times { game.roll(0) }
+    expect(game.score).to eq 24
+  end
+  it 'can play a perfect game' do
+    12.times { game.roll(10) }
+    expect(game.score).to eq 300
+  end
+
+  it 'does not allow values over 10' do
+    expect { game.roll(12) }.to raise_error 'Cannot roll values higher than 10'
+  end
+end
---
+++ spec/web_helpers.rb
@@ -0,0 +1,4 @@
+def roll_spare
+  game.roll(6)
+  game.roll(4)
+end
